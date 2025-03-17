c6c1b23f53129112a0e395bfa567373d70bcfb28
likanug <likanug.g@qq.com>
2025-03-17 17:07:09 +0800

    添加“最小栈”的解题代码和测试用例

---
+++ stack_and_queue.py
@@ -0,0 +1,53 @@
+class MinStack:
+    """
+    描述：设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+    """
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, num: int):
+        self.stack.append(num)
+        # 如果 min_stack为空或者stack的栈顶元素小于min_stack的栈顶元素
+        if not self.min_stack or self.stack[-1] <= self.min_stack[-1]:
+            self.min_stack.append(num)
+
+    def pop(self) -> None:
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def get_min(self) -> int:
+        return self.min_stack[-1]
+
+
+import unittest
+
+
+class TestMinStack(unittest.TestCase):
+
+    def setUp(self):
+        self.min_stack = MinStack()
+        self.min_stack.push(1)
+        self.min_stack.push(2)
+        self.min_stack.push(3)
+        self.min_stack.push(4)
+        self.min_stack.push(5)
+
+    def test_get_min(self):
+        self.assertEqual(self.min_stack.get_min(), 1)
+        self.min_stack.push(0)
+        self.assertEqual(self.min_stack.get_min(), 0)
+
+    def test_top(self):
+        self.assertEqual(self.min_stack.top(), 5)
+        self.min_stack.push(0)
+        self.assertEqual(self.min_stack.top(), 0)
+
+    def test_pop(self):
+        self.assertEqual(self.min_stack.top(), 5)
+        self.min_stack.pop()
+        self.assertEqual(self.min_stack.top(), 4)
